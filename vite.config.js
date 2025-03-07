e1b2a085f67d57ab1c346d618797f36aa0078016
Sara Garzón <saragarzongaldeano@gmail.com>
2025-03-07 18:20:08 +0100

    v1 ejer 12

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+export default defineConfig({
+   appType: 'mpa', // Indica que es un proyecto multipágina
+   base: './',
+   root: __dirname, 
+   build: {
+      outDir: 'produccion',
+      emptyOutDir: true,    //Vacía el directorio de producción antes de compilar
+      rollupOptions: {
+         input: {
+         main: resolve(__dirname, 'index.html'),
+         carta: resolve(__dirname, 'fuente/html/carta.html'),
+         pedidos: resolve(__dirname, 'fuente/html/pedidos.html'),
+         cafes: resolve(__dirname, 'fuente/html/cafes.html'),
+         },
+      },
+      minify: 'terser', // Asegura que JS y CSS también se minimicen
+   },
+   plugins: [
+      createHtmlPlugin({
+         minify: true, // Minimiza el HTML
+      }),
+   ],
+   server: {
+      open: true,
+   },
+   preview: {
+      open: true, // Abre automáticamente la previsualización
+      port: 4173, // Puerto para el servidor de previsualización
+   },
+});
